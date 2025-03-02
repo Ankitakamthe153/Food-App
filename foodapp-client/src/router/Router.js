c7852241736b1be30a46dbe43b964a90285eb6ca
Ankitakamthe153 <88307915+Ankitakamthe153@users.noreply.github.com>
2025-03-02 06:45:43 +0000

    Removed folder_name from repository

--- foodapp-client/src/router/Router.js
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-const router = createBrowserRouter([
-    // users
-    {
-      path: "/",
-      element: <h1>hello software developer</h1>,
-    },
-]);
-export default router;
