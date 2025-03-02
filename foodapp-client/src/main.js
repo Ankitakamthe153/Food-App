4036fee31de045784d04fca8107be86ad67ed71a
Ankitakamthe153 <88307915+Ankitakamthe153@users.noreply.github.com>
2025-03-02 10:00:20 +0000

    done

---
+++ foodapp-client/src/main.js
@@ -0,0 +1,22 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/Router.js";
+import AuthProvider from "./contexts/AuthProvider.js";
+
+// TanStack Query
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </AuthProvider>
+);
--- foodapp-client/src/pages/home/Home.js
+++ foodapp-client/src/pages/home/Home.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from 'react'
+import Banner from '../../components/Banner.js'
+import Catagories from './Catagories.js'
+import SpecialDishes from './SpecialDishes.js'
+import Testimonials from './Testimonials.js'
+import OurServices from './OurServices.js'
+
 const Home = () => {
-    return (
-      <div>
-        Home
-      </div>
-    )
-  }
-  
-  export default Home
+  return (
+    <div>
+       <Banner/>
+       <Catagories/>
+       <SpecialDishes/>
+       <Testimonials/>
+       <OurServices/>
+    </div>
+  )
+}
+
+export default Home;
