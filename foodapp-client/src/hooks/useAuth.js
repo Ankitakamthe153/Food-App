ab936e05ce818933596493ee36287b621409b465
Ankitakamthe153 <88307915+Ankitakamthe153@users.noreply.github.com>
2025-03-05 07:42:05 +0000

    Removed folder_name from repository

--- foodapp-client/src/hooks/useAuth.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { AuthContext } from "../contexts/AuthProvider.js";
-
-const useAuth = () => {
-  const auth = useContext(AuthContext);
-  return auth;
-};
-
-export default useAuth;
